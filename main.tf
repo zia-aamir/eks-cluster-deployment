ab0e394769031c66094c083568630e18507e93c9
Shah Shanediwan <aamirshanediwan@gmail.com>
2026-05-09 09:14:54 +0530

    Fix subnet_ids reference in EKS node group

--- main.tf
+++ main.tf
@@ -39,7 +39,7 @@ resource "aws_eks_node_group" "node-grp" {
   cluster_name    = aws_eks_cluster.project-cluster.name
   node_group_name = "pc-node-group"
   node_role_arn   = aws_iam_role.worker.arn
-  subnet_ids      = data.aws_subnets.available-subnets.ids
+  subnet_ids      = data.aws_subnets.available_subnets.ids
   capacity_type   = "ON_DEMAND"
   disk_size       = "20"
   instance_types  = ["t3.small"]
